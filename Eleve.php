3807995b52fbb5b0a01a83e4bb6ad1388e8d4bfb
luc.sarrazin <luc.sarrazin@free.fr>
2024-01-25 15:57:13 +0100

    Teste

--- Eleve.php
+++ Eleve.php
@@ -3,13 +3,24 @@ Class Eleve {
     private $nom ;
     private $prenom ;
 
+    public function __construct($unNom,$unPrenom){
+
+        $this->nom = $unNom;
+        $this->prenom = $unPrenom;
+    }
+
     public function Travaille()
     {
-        return "L'élève travaille comme il faut";
+        return "L'élève $this->nom $this->prenom vous travaillez comme il le faut \n";
     }
 
 }
-$mrRobert = new Eleve();
+
+$mrRobert = new Eleve("Robert","Timothée");
+$mrFlores = new Eleve("Flores","Peter");
+$mrTandia = new Eleve("Tandia","Sekou");
 echo $mrRobert->Travaille();
+echo $mrFlores->Travaille();
+echo $mrTandia->Travaille();
 
 
