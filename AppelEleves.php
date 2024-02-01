ff8f6e64609e8f81531e96bd614200113982daca
luc.sarrazin <luc.sarrazin@free.fr>
2024-02-01 14:26:12 +0100

    Teste

--- AppelEleves.php
+++ AppelEleves.php
@@ -1,1 +1,11 @@
 <?php
+include 'Eleve.php';
+
+$mrRobert = new Eleve("Robert","Timothée");
+$mrFlores = new Eleve("Flores","Peter");
+$mrTandia = new Eleve("Tandia","Sekou");
+$mrSterenn = new Eleve("Gougeon","Sterenn");
+echo $mrRobert->Travaille();
+echo $mrFlores->Travaille();
+echo $mrTandia->Travaille();
+echo $mrSterenn->Travaille();
